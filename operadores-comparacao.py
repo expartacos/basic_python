ff0e4465f70de2a03813ff1d8bceb60de15324c7
expartacos <rafaelpersan@gmail.com>
2024-01-09 20:12:07 -0300

    Operadores de comparação

---
+++ operadores-comparacao.py
@@ -0,0 +1,14 @@
+x = y = z = False 
+
+n1 = n2 = 0
+
+print('Digite um número:')
+
+n1 = int(input()) 
+
+
+print('Digite um número:')
+n2 = int(input())
+
+x = n1 == n2
+print('são igauais? ', x, '\n')
