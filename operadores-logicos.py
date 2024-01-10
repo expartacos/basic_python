d8579e53fba0d7ecd3a83152818d3bc5e3d38af8
expartacos <rafaelpersan@gmail.com>
2024-01-09 21:15:47 -0300

    Operadores logicos

---
+++ operadores-logicos.py
@@ -0,0 +1,20 @@
+# idade = 25
+# altura = 1.73
+
+# resultado = (idade >= 18) and (altura >= 1.70)
+
+# msg = 'pode participar do evento? ' + str(resultado)
+
+# print(msg)
+
+#programa disparar alarme 
+
+porta = 'f'
+
+janela = 'f'
+
+alarme = (porta == 'a') or (janela == 'a')
+
+msg = 'Alarme disparado? ' +str(alarme)
+
+print(msg)
