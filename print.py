9d3fbaab58f116ed5c91916266e0d7588b4f51b6
expartacos <rafaelpersan@gmail.com>
2024-01-09 21:45:33 -0300

    condicionais

---
+++ print.py
@@ -0,0 +1,16 @@
+#Sintaxe:
+#print(objetos, argumentos)
+
+#Exemplos:
+
+mensagem = 'Função print()'
+
+print(mensagem)
+print('Python')
+
+nome = 'Rafael'
+
+print ('Bem vindo,', nome)
+
+name = input('por favor, digite seu nome: ')
+print('Olá ' + name + '! Bem-Vindo ao Python!')
