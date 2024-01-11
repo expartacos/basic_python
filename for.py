136cbb80c3c67f2bb4943c4739e99c66cbbb7d38
expartacos <rafaelpersan@gmail.com>
2024-01-11 20:36:54 -0300

    laço for

---
+++ for.py
@@ -0,0 +1,22 @@
+# num = 1
+
+# while (num <= 10):
+#     print(num)
+#     num += 1
+
+# print('Acabou!')
+
+# for numero in range(1, 11):
+#     print(numero)
+
+#a função range (valor_inicial, valor_final,)
+
+# for x in range (2, 21, 3):
+#     print (x)
+
+pedras = ('rubio', 'esmeralda', 'safira', 'ametista')
+
+for pedra in pedras: 
+    if pedra == 'safira':
+        continue
+    print(pedra)
