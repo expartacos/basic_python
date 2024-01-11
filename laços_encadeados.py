adcaf67a97c6ee7ca2098180f2cd193f0ce665a9
expartacos <rafaelpersan@gmail.com>
2024-01-11 20:43:49 -0300

    encadeamento de laços

---
+++ laços_encadeados.py
@@ -0,0 +1,13 @@
+import random
+# for cont_ex in range(1,6):
+#     print(f'\nRodada: {cont_ex}')
+#     for cont_int in range(5,0,-1):
+#         print(f'valor: {cont_int}')
+# print('Acabou!')
+
+for A in range(1,6):
+    print(f'\nconjunto: {A}')
+    for B in range(5):
+        num = random.randint(1,100)
+        print(f'valor: {num}')
+    
