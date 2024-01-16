908baf9dc04c412b4047ec432dfff88601f8123c
expartacos <rafaelpersan@gmail.com>
2024-01-16 10:58:32 -0300

    Metodo math

---
+++ matemática.py
@@ -0,0 +1,23 @@
+import math
+
+# Funções builtin (funções internas)
+
+# valores = [2, 4, 5, 6, 3]
+# print(max(valores))
+# print(min(valores))
+# print(sum(valores))
+# print(abs(-10))
+# print(pow(-5, 4))
+# print(round(1.2345555, 3))
+
+x = 8
+y = 100
+
+raiz_quadrada = math.sqrt(x)
+print(raiz_quadrada)
+
+logaritmo = math.log10(x)
+print(logaritmo)
+
+print(math.pi)
+print(math.factorial(y))
