3d58c88f9b483f700083f6639daeb908dcd041d2
expartacos <rafaelpersan@gmail.com>
2024-01-17 21:45:20 -0300

    String

--- strings.py
+++ strings.py
@@ -10,3 +10,32 @@ frase = 'Vamos aprender Python hoje'
 # dominio = email[arroba+1:]
 # print(usuario)
 # print(dominio)
+
+# produtos = 'carbonato de sódio e óxido de zinco'
+# print('sódio' in produtos)
+# print('magnésio' not in produtos)
+
+# intem = 'hipoclorito'
+# pos = intem.find('clor')
+# print(pos)
+# pos = intem.find('flus')
+# print(pos)
+# objeto_celeste = 'Galáxias esPriral M31'
+# print(objeto_celeste.upper())
+# print(objeto_celeste.lower())
+# print(objeto_celeste.capitalize())
+# print(objeto_celeste.title())
+
+# suplemento = 'cloreto de magnésio'
+# n_suplemento = suplemento.replace('magnésio', 'sodio')
+# print(n_suplemento)
+
+# frase = '        Ômega 3 é bom para a saúde     '
+# print(frase.lstrip())
+# print(frase.rstrip())
+# print(frase.strip())
+
+fruta = 'banana'
+print(fruta)
+print(fruta.rjust(20))
+print(fruta.center(20, '-'))
