f5b99148b6886958270d2677cf322b211151d2b8
expartacos <rafaelpersan@gmail.com>
2024-01-17 21:16:23 -0300

    String

--- strings.py
+++ strings.py
@@ -1,28 +1,3 @@
-# Strings são imutaveis / mas as variaveis que contem as strings são mutaveis 
-# nome = 'Estudo de python'
-# aluno = 'Rafael'
-# letra = nome[2]
-# print(letra)
-# print(len(nome))
-# print(nome + ' ' + aluno)
-# palavras = nome.split()
-# print(palavras)
-
-# for palavra in palavras:
-#     print(palavra)
-
-# email = input('Digite seu email: ')
-# arroba = email.find('@')
-# print(arroba)
-# usuario = email[0: arroba]
-# dominio = email[arroba + 1:]
-# print(usuario)
-# print(dominio)
-
-# produtos = 'carbonato de sódio e óxido de zinco'
-# print('sódio ' in produtos)
-# print('magnésio ' not in produtos)
-
-objeto_celeste = 'galáxia espiral M31'
-print(objeto_celeste.capitalize())
-print(objeto_celeste.title())
+frase = 'Vamos aprender Python hoje'
+palavras = frase.split()
+print(palavras)
