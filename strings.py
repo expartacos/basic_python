d0c561ec8543749d54c916bc38ce10db6409d517
expartacos <rafaelpersan@gmail.com>
2024-01-17 21:33:46 -0300

    String

--- strings.py
+++ strings.py
@@ -1,5 +1,12 @@
 frase = 'Vamos aprender Python hoje'
-palavras = frase.split()
-print(palavras)
-for letra in frase:
-    print(frase)
+# palavras = frase.split()
+# print(palavras)
+# for letra in frase:
+#     print(frase)
+# email = input('Digite seu endereço de email: ')
+# arroba = email.find('@')
+# print(arroba)
+# usuario = email[0:arroba]
+# dominio = email[arroba+1:]
+# print(usuario)
+# print(dominio)
