5a7fde1f08b9bbc391a1cbdc4bab71166c288399
expartacos <rafaelpersan@gmail.com>
2024-01-19 21:47:43 -0300

    sets

---
+++ sets.py
@@ -0,0 +1,15 @@
+planeta_anao = {'plutão', 'Ceres', 'Eris'}
+print(planeta_anao)
+
+# for astro in planeta_anao:
+#     print(astro.upper(), end='')
+
+# astro = ['lua', 'marte', 'plutão', 'lua']
+# print(astro)
+
+# astro_set = set(astro)
+# print(astro_set)
+
+astro1 = {'plutão', 'Ceres', 'Eris'}
+astro2 = {'lua', 'marte', 'plutão', 'lua'}
+print(astro1 | astro2)
