8281ea5f90b454339ede3934fd6bf137fb0f856c
expartacos <rafaelpersan@gmail.com>
2024-01-22 15:20:56 -0300

    funções

--- funções.py
+++ funções.py
@@ -11,3 +11,24 @@
 #     print(a + b)
 
 # soma(12, 7)
+
+# def mult(x, y):
+#     return x*y
+
+# a = 5
+# b = 6
+# c = mult(a, b)
+# print(c)
+
+def div(x, y):
+    if y != 0:
+        return x/y
+    else:
+        return 'Erro'
+    
+if __name__ == '__main__':
+    a = int(input('Primeiro valor: '))
+    b = int(input('Segundo valor: '))
+
+    c = div(a, b)
+    print(f'{a} dividido por {b} é = {c}')
