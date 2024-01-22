1ba47a94ed5067fe4f748cc9f768943d13363629
expartacos <rafaelpersan@gmail.com>
2024-01-22 14:58:23 -0300

    funções

---
+++ funções.py
@@ -0,0 +1,13 @@
+# Funções 
+# Modularização, Reúso de código, Legibilidade,
+
+# def mensagem():
+#     print('Ola mundo!')
+
+# mensagem()
+
+# Função com argumentos
+# def soma(a, b):
+#     print(a + b)
+
+# soma(12, 7)
