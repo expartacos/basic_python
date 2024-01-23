6130e9a89f607f77f9a57ebb9ddeae4f7f3f5bce
expartacos <rafaelpersan@gmail.com>
2024-01-23 13:14:30 -0300

    Escopo

---
+++ escopo.py
@@ -0,0 +1,19 @@
+# Escopo Global e Escopo Local 
+
+# Variáveis globais são declaradas e da pra usar em toda parte do código 
+# Variáveis locais são declaradas e da pra usar apenas dentro do bloco onde foi declarada
+
+var_global = 'Global' # Variável global
+
+def escreve_texto():
+    var_local = 'Local' # Variável local
+    print(f'Variável local: {var_local}')
+    print(f'Variável global: {var_global}')
+
+if __name__ == '__main__':
+    print(f'Executar a função escreve_texto()')
+    escreve_texto()
+
+    print('Tentar acessar as variáveis diretamente')
+    # print(f'Variável local: {var_local}')
+    print(f'Variável global: {var_global}')
