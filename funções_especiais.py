40520371c316bf5e4aa3be71448b2f6e949effce
expartacos <rafaelpersan@gmail.com>
2024-01-25 20:05:57 -0300

    'map'

--- funções_especiais.py
+++ funções_especiais.py
@@ -2,7 +2,21 @@
 # Sintaxe: 
 # lambda argumentos: expressão
 
-quadrado = lambda x: x**2
+# quadrado = lambda x: x**2
 
-for i in range(1, 11):
-    print(quadrado(i))
+# for i in range(1, 11):
+#     print(quadrado(i))
+
+# par = lambda x: x % 2 == 0
+# print(par(9))
+
+# f_c = lambda f: (f-32)*5/9
+# print(f_c(212))
+
+# Função map()
+# Sintaxe:
+# map(função, iterável)
+
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+dobro = list(map(lambda x: x*2, num))
+print(dobro)
