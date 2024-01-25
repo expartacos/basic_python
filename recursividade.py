e99803f03afbb823fb6f0b574824b13af45b07e2
expartacos <rafaelpersan@gmail.com>
2024-01-25 19:47:34 -0300

    'fatorial'

---
+++ recursividade.py
@@ -0,0 +1,6 @@
+# Recursividade 
+
+# Fórmula geral para o fatorial:
+# fatorial(num) = 1, se num = 0 ou se num = 1 'Caso-Base'
+# fatorial(num) = num * fatorial(num-1), se num > 1 'Caso Recursivo'
+# fatorial 4! = 4 * fatorial(3)  = 4 * 3 * fatorial(2) = 4 * 3 * 2 * fatorial(1) = 4 * 3 * 2 * 1 = 24
